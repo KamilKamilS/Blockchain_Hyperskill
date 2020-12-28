c33e638600f8e5c61a5a990885e69af35c5c0388
Kamil <s.kamilx@gmail.com>
2020-12-28 17:32:31 +0100

    Block class created

--- Blockchain/task/src/blockchain/Block.java
+++ Blockchain/task/src/blockchain/Block.java
@@ -1,2 +1,16 @@
-package blockchain;public class Block {
+package blockchain;
+
+public class Block {
+
+    int id;
+    long timestamp;
+    String previousHash;
+    String currentHash;
+
+    public Block(int id, long timestamp, String previousHash, String currentHash) {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.previousHash = previousHash;
+        this.currentHash = currentHash;
+    }
 }
